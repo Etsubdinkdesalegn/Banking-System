ea1d9acaf8bea42d31a43d820e0ca39496a25660
Etsubdink <etsubdinkdesalegnadamu@gmail.com>
2026-05-16 15:42:52 -0700

    Fix: Resolve '@components/ui/card' module not found and type errors

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
@@ -23,7 +25,7 @@ const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 )
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...props }, ref) => (
     <h3
       ref={ref}
